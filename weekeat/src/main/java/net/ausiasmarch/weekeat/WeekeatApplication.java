72e22a35bb230297725b7b514a2933a290197b2c
Nieves <smilenvs@gmail.com>
2024-01-10 18:22:04 +0100

    Primer commit server (aún no hay nada)

---
+++ weekeat/src/main/java/net/ausiasmarch/weekeat/WeekeatApplication.java
@@ -0,0 +1,13 @@
+package net.ausiasmarch.weekeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeekeatApplication.class, args);
+	}
+
+}
---
+++ weekeat/src/test/java/net/ausiasmarch/weekeat/WeekeatApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ausiasmarch.weekeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeekeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
