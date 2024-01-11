e046dc4c3b35788ff53f402ba24efe1bfbcbda4d
Nieves <smilenvs@gmail.com>
2024-01-11 19:14:14 +0100

    db

--- bbdd/weekeat_db.sql
+++ bbdd/weekeat_db.sql
@@ -17,6 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+CREATE DATABASE IF NOT EXISTS `weekeat_db`;
+USE `weekeat_db`;
 --
 -- Base de datos: `weekeat_db`
 --
