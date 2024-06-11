fc37a3651b297bb64e0f756e4e6a04136c4b8918
Nieves Navas Aguilar <95766880+Nievesnagui@users.noreply.github.com>
2024-06-11 13:08:11 +0200

    Update weekeat_db.sql

--- bbdd/weekeat_db.sql
+++ bbdd/weekeat_db.sql
@@ -17,6 +17,9 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+CREATE DATABASE IF NOT EXISTS `weekeat_db`;
+USE `weekeat_db`;
+
 --
 -- Base de datos: `weekeat_db`
 --
